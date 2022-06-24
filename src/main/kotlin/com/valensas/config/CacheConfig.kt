9b36089e1ae7af2536f242899d801e26b7603860
oserifogullari18 <oserifogullari18@ku.edu.tr>
2022-06-24 14:26:08 +0300

    bucket4j and test

---
+++ src/main/kotlin/com/valensas/config/CacheConfig.kt
@@ -0,0 +1,6 @@
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableCaching
+class CacheConfig
--- src/test/kotlin/com/valensas/RateLimiterApplicationTests.kt
+++ src/test/kotlin/com/valensas/RateLimiterApplicationTests.kt
@@ -1,12 +1,40 @@
 package com.valensas
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class RateLimiterApplicationTests {
+    @Autowired
+    lateinit var restTemplate: TestRestTemplate
+
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     @Test
     fun contextLoads() {
+        val limit = 5
+        (0..10).forEach {
+            val response = restTemplate.exchange("/test", HttpMethod.GET, null, String::class.java)
+            logger.info("Request: {} - Status: {}", it, response.statusCode)
+            val expectedStatusCode = if (it < limit) HttpStatus.OK else HttpStatus.TOO_MANY_REQUESTS
+            assertEquals(expectedStatusCode, response.statusCode)
+            if (expectedStatusCode == HttpStatus.OK) {
+                val remaining = response.headers["X-Rate-Limit-Remaining"]?.firstOrNull()
+                logger.info("Remaining limit: {}", remaining)
+                assertNotNull(remaining)
+                assertEquals(limit - it - 1, remaining!!.toInt())
+            } else {
+                val remainingTime = response.headers["X-Rate-Limit-Retry-After-Seconds"]?.firstOrNull()
+                logger.info("Remaining time: {}", remainingTime)
+            }
+        }
     }
 }
