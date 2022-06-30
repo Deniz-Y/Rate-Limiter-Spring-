ce82708508e3f1ccf5d09da1e7c0dea553822211
Can Taylan Capraz <taylan@valensas.com>
2022-06-30 10:04:56 +0300

    add x-forwarded-for filter to rate limiter

--- src/main/kotlin/com/valensas/controller/Test2Controller.kt
+++ src/main/kotlin/com/valensas/controller/Test2Controller.kt
@@ -1,6 +1,5 @@
 package com.valensas.controller
 
-
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
--- src/test/kotlin/com/valensas/RateLimiterApplicationTests.kt
+++ src/test/kotlin/com/valensas/RateLimiterApplicationTests.kt
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import java.lang.Thread.sleep
@@ -41,16 +43,45 @@ class RateLimiterApplicationTests {
                 }
             }
         }
-        var longTime = (intTime*1000).toLong() + 1000
+        var longTime = (intTime * 1000).toLong() + 1000
         logger.info("Sleep {} seconds", intTime + 1)
         sleep(longTime)
         val response = restTemplate.exchange("/test", HttpMethod.GET, null, String::class.java)
-        logger.info("Request after sleep - Status: {}",  response.statusCode)
+        logger.info("Request after sleep - Status: {}", response.statusCode)
         assertEquals(HttpStatus.OK, response.statusCode)
     }
 
     @Test
-    fun headerTest(){
+    fun `apply ip based rate limit`() {
+        val limit = 5
+        val ip1 = "1.1.1.1"
+        val ip2 = "1.1.1.2"
 
+        // Send request from ip1 and get OK
+        repeat(limit) { call(ip1, HttpStatus.OK) }
+        // Get 429 if the rate limit is exceeded
+        call(ip1, HttpStatus.TOO_MANY_REQUESTS)
+
+        // Send request from ip2 and get OK
+        repeat(limit) { call(ip2, HttpStatus.OK) }
+        // Get 429 if the rate limit is exceeded
+        call(ip2, HttpStatus.TOO_MANY_REQUESTS)
+
+        // Send request from both ip addresses and get 429
+        listOf(ip1, ip2).forEach { ip -> call(ip, HttpStatus.TOO_MANY_REQUESTS) }
+    }
+
+    fun call(ip: String, expectedStatus: HttpStatus) {
+        logger.info("Sending request from {}. Expected response status: {}", ip, expectedStatus)
+        val headers = HttpHeaders()
+        headers.add("X-FORWARDED-FOR", ip)
+        val entity: HttpEntity<String> = HttpEntity(null, headers)
+        val response = restTemplate.exchange("/test", HttpMethod.GET, entity, String::class.java)
+        logger.info("Response status of the request from {} is {}", ip, expectedStatus)
+        assertEquals(expectedStatus, response.statusCode)
+    }
+
+    @Test
+    fun headerTest() {
     }
 }
